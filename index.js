6ac32d1cb2ae1a13e0cf85c8a4f87afa7c4d5811
el que m'est <14348482+elquimista@users.noreply.github.com>
2017-06-27 16:26:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Koa = require('koa');
+const parseBody = require('koa-body');
+const Router = require('koa-router');
+const requestify = require('requestify');
+const cors = require('koa2-cors');
+
+const app = new Koa();
+const router = new Router();
+
+router
+  .post('/gitlab/oauth/token', async (ctx, next) => {
+    const { client_id, client_secret, grant_type, redirect_uri, code } = ctx.request.body;
+    try {
+      const response = await requestify.post('https://gitlab.com/oauth/token', {
+        client_id, client_secret, grant_type, redirect_uri, code
+      });
+      ctx.body = response.getBody();
+    } catch (err) {
+      ctx.body = err.getBody();
+      ctx.status = 401;
+    }
+    return next();
+  });
+
+app.use(cors());
+app.use(parseBody());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(4000);
